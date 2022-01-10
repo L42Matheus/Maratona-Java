e7749c61fe138f1813e7af1de3efd2e73e3be3cb
L42Matheus <lmatheus.tome@gmail.com>
2022-01-10 19:45:26 -0300

    Arrays Multidimensionais, foreach

---
+++ src/main/java/Aula08ArraysMultidimensionais02.java
@@ -0,0 +1,19 @@
+public class Aula08ArraysMultidimensionais02 {
+    public static void main(String[] args) {
+        int[][] arrayInt = new int[3][];
+
+        arrayInt[0] = new int[2];
+        arrayInt[1] = new int[3];
+        arrayInt[2] = new int[4];
+
+        // inicializando array de forma diferente.
+        int [][] arrayInt2 = {{0,0}, {1,2,3}, {1,2,3,4,5,6}};
+
+        for (int [] arrayBase: arrayInt) {
+            for (int num: arrayBase) {
+                System.out.println(num);
+            }
+        }
+
+    }
+}
